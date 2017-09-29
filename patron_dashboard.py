7912982c71a6fbbfe9574c496ef1ecef12017b6b
Steven Nguyen <stevenan@live.unc.edu>
2017-09-29 16:24:09 -0400

    added logging to patron scripts

--- patron_dashboard.py
+++ patron_dashboard.py
@@ -22,12 +22,18 @@ def create_csv(writer):
     # first id is 100010
     id = 100010
 
+    log_file.write("Getting Access token for authentication. \n")
+    token = get_token()
+    log_file.write("Token retrieved.\n\n")
+    
+    log_file.write("Sending GET request, accessing 2000 id's at a time. \n")
+    
     # loop until there are no more patron records (error code 200)
     while True:
         
         url = "https://catalog.chapelhillpubliclibrary.org/iii/sierra-api/v3/patrons?limit=2000&deleted=false&fields=expirationDate,addresses,names,emails&id=["+str(id)+",]"
         request = requests.get(url, headers={
-                    "Authorization": "Bearer " + get_token()
+                    "Authorization": "Bearer " + token
                 })
                 
         if request.status_code != 200:
@@ -52,19 +58,39 @@ def create_csv(writer):
             except KeyError:
                 continue
         
+        id_prev = id
         id = jfile["entries"][-1]["id"] + 1
         
-        print(id)
+        log_file.write("Records from id's " + str(id_prev) + " and on written to csv file, Accessing next page.\n")
+        # print(id)
         
+
+# create date variable
+today = datetime.date.today()
+
+# throw an error if a "/logs" directory doesn't exist
+try:
+    log_file = open('logs/' + str(today) + '-expired_patrons.txt', 'w')
+except:
+    error_file = open('error.txt', 'w')
+    error_file.write('ERROR - "logs" directory not found\n')
+    error_file.close()
+    
 # open csv file for writing
+log_file.write('Creating expired_patrons.csv file.\n\n')
 expired_patrons = open('//CHFS/Shared Documents/OpenData/datasets/unpublished/expired_patrons.csv', 'w')
 
 # create a csvwriter object
 csvwriter = csv.writer(expired_patrons)
 
 # write a header & call the create_csv function
+log_file.write('Writing header on expired_patrons file. \n\n')
 csvwriter.writerow(['names','addresses','emails','expirationDate'])
 create_csv(csvwriter)
 
-# close file
+log_file.write("\nAll expired patron data has been successfully written to expire_patrons.csv.\n\n")
+log_file.write(str(datetime.datetime.now()))
+
+# close files
 expired_patrons.close()
+log_file.close()
